ae469de8272c37298fe67fb139517621e11fd484
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-03-22 12:24:43 +0100

    PdfEncryptor: make it work

---
+++ java.src/net/safester/application/util/PdfEncryptor.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.safester.application.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import org.apache.commons.io.IOUtils;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+
+public class PdfEncryptor {
+
+    /**
+     * Takes a PDF stream (typically from memory) and encrypts it with a password. The encrypted PDF is then
+     * saved to the specified output path. This method uses Apache PDFBox and Apache Commons IO libraries.
+     *
+     * @param inputPdfStream The InputStream of the PDF to be encrypted.
+     * @param outputPath     The file path where the encrypted PDF should be saved.
+     * @param userPassword   The password required to open/view the PDF. Must not be null.
+     * @param ownerPassword  The password that gives full control over the PDF. Must not be null.
+     * @throws IOException If an error occurs during processing the PDF or saving the file.
+     */
+    
+    public static void encryptPdf(InputStream inputPdfStream, String outputPath, String userPassword, String ownerPassword) throws IOException {
+        Objects.requireNonNull(userPassword, "User password must not be null");
+        Objects.requireNonNull(ownerPassword, "Owner password must not be null");
+
+        byte[] streamAsByteArray = IOUtils.toByteArray(inputPdfStream);
+        PDDocument document = Loader.loadPDF(streamAsByteArray);
+
+        AccessPermission accessPermission = new AccessPermission();
+
+        // Set to true if you want to allow printing
+        accessPermission.setCanPrint(false);
+        accessPermission.setCanExtractContent(false);
+
+        StandardProtectionPolicy spp = new StandardProtectionPolicy(ownerPassword, userPassword, accessPermission);
+        spp.setEncryptionKeyLength(128);
+        document.protect(spp);
+
+        //ByteArrayInputStream bis = new ByteArrayInputStream(streamAsByteArray);
+        File output = new File(outputPath);
+        document.save(output);
+
+    }
+}
---
+++ src/test/java/net/safester/application/util/test/PdfEncryptorTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.safester.application.util.test;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Date;
+import net.safester.application.util.PdfEncryptor;
+
+/**
+ *
+ * @author ndepo
+ */
+public class PdfEncryptorTest {
+
+    public static void main(String args[]) throws Exception {
+        System.out.println(new Date() + " " + "Begin..." );
+        String inputPath = "c:\\tmp\\Safester_Cybtech_dev_1.0.pdf";
+        String outputPath = "c:\\tmp\\Safester_Cybtech_dev_1.0._ENCRYPTED.pdf";
+        
+        try (InputStream in = new BufferedInputStream(new FileInputStream(inputPath));) {
+             PdfEncryptor.encryptPdf(in, outputPath, "test", "test");
+        }
+        
+        System.out.println(new Date() + " " + "End." );
+
+    }
+}
